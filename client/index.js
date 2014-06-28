7e0eaf8474f7fb893e7de47ee22690e84c97f93f
Stead68 <j0rd4n68@gmail.com>
2014-06-28 19:42:41 +0200

    [v0.11] Uploading the project.

---
+++ client/index.js
@@ -0,0 +1,199 @@
+// Settings
+var ip = 'hashcat-pool.no-ip.org'; // IP of pool
+var port = '1337'; // Port of pool
+
+var exec = require('child_process').exec;
+var io = require('socket.io-client')('http://' + ip + ':' + port);
+var fs = require('fs');
+var os = require('os');
+
+var child;
+
+var currenthash;
+var speed = 0;
+
+var system = os.type();
+
+var version = 11;
+
+io.on('connect', function(socket) {
+	console.log(dateFormat() + "[CLIENT] Connected to pool.");
+	console.log(dateFormat() + "[CLIENT] Logging in.");
+	
+	io.emit('worker-login', {
+		system: system,
+		version: version
+	});
+
+	io.on('worker-logged', function() {
+		console.log(dateFormat() + "[CLIENT] Requesting block from pool.");
+		io.emit('block-request');
+	});
+
+	io.on('block-received', function(blockdata) {
+		console.log(dateFormat() + "[CLIENT] Block received.");
+
+		if (currenthash != blockdata.hashtodecrypt)
+			console.log(dateFormat() + "[HASH] " + blockdata.hashtodecrypt);
+
+		currenthash = blockdata.hashtodecrypt;
+
+		var hash = blockdata.hashtodecrypt;
+		var hashtype = blockdata.hashtype;
+		var attackmode = blockdata.attackmode;
+		var blockstart = blockdata.blockstart;
+		var blocksize = blockdata.blocksize;
+
+		if (fs.existsSync("hashcat/hash.txt"))
+			fs.unlinkSync("hashcat/hash.txt");
+
+		fs.writeFile("hashcat/hash.txt", hash, function(err) { });
+
+		if (fs.existsSync("hashcat/hashcat-cli32.exe") && fs.existsSync("hashcat/hashcat-cli64.exe") && fs.existsSync("hashcat/hashcat-cli32.bin") && fs.existsSync("hashcat/hashcat-cli64.bin") && fs.existsSync("hashcat/hashcat-cli64.app"))
+		{
+			var command = '"hashcat/hashcat-cli64.exe" -m ' + hashtype + ' -a ' + attackmode + ' -s ' + blockstart + ' -l ' + blocksize + ' "hashcat/hash.txt" ?a?a?a?a?a?a?a?a?a?a?a?a?a';
+
+			var fail = false;
+
+			console.log(dateFormat() + "[CLIENT] Started bruteforcing process.");
+			child = exec(command, function(error, stdout, stderr) { 
+				if (error === null && stderr == "")
+				{
+						speed = parseSpeed(stdout);
+
+						if (stdout.indexOf("Started:") == -1 || stdout.indexOf("Stopped:") == -1)
+							fail = true;
+				}
+				else
+					fail = true;
+			});
+
+			child.on('close', function(code) {
+				if (!fail)
+				{
+					if (!hashFound())
+					{
+						console.log(dateFormat() + "[CLIENT] No hash discovered, requesting block from pool.");
+
+						io.emit('block-completed', blockstart);
+					}
+					else
+					{
+						var result = decrypted(); drop();
+
+						console.log(dateFormat() + "[CLIENT] Hash discovered: " + result + ".");
+						
+						io.emit('hash-found', result);
+					}
+				}
+				else
+				{
+					console.log(dateFormat() + "[CLIENT] Error while computing the block.");
+					io.emit('block-error');
+				}
+			});
+		}
+		else
+		{
+			console.log(dateFormat() + "[CLIENT] Cannot bruteforce block, hashcat is missing.");
+		}
+	});
+
+	io.on('block-ready', function() {
+		io.emit('block-request', { speed: speed });
+	});
+
+	io.on('block-aborted', function() {
+		console.log(dateFormat() + "[CLIENT] Pool stopped the workers (Someone decrypted the hash).");
+
+		io.emit('block-request');
+	});
+
+	io.on('hash-aborted', function() {
+		console.log(dateFormat() + "[CLIENT] Pool stopped the workers (Hash is too hard).");
+
+		io.emit('block-request');
+	});
+
+	io.on('message-received', function(message) {
+		console.log(dateFormat() + message);
+	});
+
+	io.on('bruteforce-completed', function() {
+		console.log(dateFormat() + "[CLIENT] All hashes had been discovered.");
+	});
+
+	io.on('disconnect', function(){
+		console.log(dateFormat() + "[CLIENT] Connection lost with pool.");
+	});
+});
+
+function hashFound() {
+	if (!fs.existsSync("hashcat.pot"))
+			fs.writeFileSync("hashcat.pot", "");
+
+	var file = fs.readFileSync("hashcat.pot").toString().trim();
+
+	var results = file.split('\n');
+
+	if (results.length > 0)
+	{
+		if (results[results.length - 1].indexOf(currenthash.trim()) > -1)
+			return true;
+		else
+			return false;
+	}
+	else
+		return false;
+}
+
+function decrypted() {
+	var file = fs.readFileSync("hashcat.pot").toString().trim();
+
+	var results = file.split('\n');
+
+	if (results.length > 0)
+	{
+		var result = results[results.length - 1].split(':')[2].split('\n')[0].trim();
+
+		return result;
+	}
+	else
+		return "";
+}
+
+function drop() {
+	if (fs.existsSync("hashcat.pot"))
+		fs.unlinkSync("hashcat.pot");
+}
+
+function parseSpeed(string) {
+
+	var speed;
+	var realspeed;
+
+	speed = string.substring(string.indexOf("plains,") + 8, string.length);
+	speed = speed.substring(0, speed.indexOf(" words"));
+	
+	realspeed = parseFloat(speed.trim());
+
+	if (speed.indexOf("k") > -1)
+		realspeed = realspeed * 1000;
+	else if (speed.indexOf("M") > -1)
+		realspeed = realspeed * 1000000;
+	else if (speed.indexOf("G") > -1)
+		realspeed = realspeed * 1000000000;
+
+	if (!isNaN(realspeed))
+	{
+		return realspeed;
+	}
+	else
+		return 0;
+}
+
+function dateFormat() {
+	var date = new Date();
+
+	return "[" + date.getMonth() + "-" + date.getDate() + "-" + date.getFullYear() + " " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + "]";
+}
---
+++ server/index.js
@@ -0,0 +1,396 @@
+var http = require('http');
+var fs = require('fs');
+var crypto = require('crypto');
+
+var port = 1337;
+
+var app = http.createServer(function(request, response) {
+	response.end("Hey! There is nothing to see here ;)");
+});
+
+app.listen(port);
+
+var io = require("socket.io").listen(app);
+
+var workers = { };
+var unresolvedblocks = [ ];
+var inprogressblocks = [ ];
+
+var hashindex = 13; // Edit this line to start at a specific line
+var hash = nextHash();
+var blockstart = 0; // Edit this line to start at a specific block
+var blocksize = 1500000000; // Higher value require more time
+
+var maxresolvetime = 1800; // In seconds
+
+// Auto-skip hash
+// 95 = 1 char-len
+// 9025 = 2 char-len
+// 857375 = 3 char-len
+// 81450625 = 4 char-len
+// 7737809375 = 5 char-len
+// 735091890625 = 6 char-len
+// 69833729609375 = 7 char-len
+// 6634204312890625 = 8 char-len
+// 630249409724609375 = 9 char-len
+// 59873693923837890625 = 10 char-len
+
+var autoskip = 735091890625; 
+
+var poolspeed = 0;
+var onlines = 0;
+
+var serverprotocol = 11;
+
+console.log("[SERVER] Server is running on port " + port + " with protocol support: " + serverprotocol);
+
+io.sockets.on("connection", function(socket) {
+
+	updateStats(io);
+
+	socket.on('worker-login', function(data) {
+
+		if (typeof(data) !== typeof(undefined))
+		{
+			if (typeof(data.version) !== typeof(undefined) && typeof(data.system) !== typeof(undefined))
+			{
+				if (data.version == serverprotocol)
+				{
+					if (verifyOs(data.system))
+					{
+						console.log(dateFormat() + "[SERVER] Incomming connection from worker id: " + socket.id + ". (" + data.system + ")");
+			
+						workers[socket.id] = new worker(socket);
+
+						socket.emit('worker-logged');
+					}
+					else
+					{
+						console.log(dateFormat() + "[SERVER] Rejected worker id: " + socket.id + ". Invalid OS: " + data.system);
+						
+						socket.emit('message-received', "[OS] Your os is blacklisted, please check the website.");
+						socket.disconnect();
+					}
+				}
+				else
+				{
+					socket.emit('message-received', "[UPDATE] Your client is outdated, please check the website.");
+					socket.disconnect();
+
+					// TO-DO: Send auto update packet to client.
+				}
+			}
+		}
+	});
+
+	socket.on('block-request', function(data) {
+		if (blockstart <= autoskip || Object.keys(unresolvedblocks).length > 0)
+		{
+			if (typeof(workers[socket.id]) !== typeof(undefined))
+			{
+				if (workers[socket.id].ready)
+				{
+					if (Object.keys(unresolvedblocks).length > 0) {
+
+						var claim = unresolvedblocks.shift(); // Return the first element and delete it.
+					}
+					else {
+						var claim = blockstart;
+						blockstart += blocksize;
+					}
+
+					if (typeof(data) !== typeof(undefined))
+						if (!isNaN(data.speed))
+							if (data.speed > 0)
+								workers[socket.id].speed = data.speed;
+
+					inprogressblocks[Object.keys(inprogressblocks).length] = claim;
+
+					updateStats(io);
+
+					sendBlock(socket, hash, claim, blocksize);
+					console.log(dateFormat() + "[SERVER] Block " + claim + " sent to " + socket.id + ".");
+				}
+			}
+		}
+		else
+		{
+			if (Object.keys(inprogressblocks).length == 0)
+			{
+				// Load next hash
+				saveAbortedHash(hash);
+				hash = nextHash();
+				
+				io.sockets.emit('hash-aborted');
+			}
+			else
+			{
+				io.sockets.emit('message-received', "[SERVER] Waiting for results of " + Object.keys(inprogressblocks).length + " client(s).");
+			}
+		}
+	});
+
+	socket.on('block-completed', function(blockstart) {
+
+		for (var k in inprogressblocks)
+		{
+			if (inprogressblocks[k] == blockstart)
+				inprogressblocks.splice(k, 1);
+		}
+
+		clearTimeout(workers[socket.id].timer[blockstart]);
+
+		delete workers[socket.id].blockclaimed;
+		
+		workers[socket.id].ready = true;
+		workers[socket.id].errors = 0;
+
+		socket.emit('block-ready');
+	});
+
+	socket.on('block-error', function() {
+
+		workers[socket.id].errors++;
+
+		if (workers[socket.id].errors <= 5)
+		{
+			console.log(dateFormat() + "[SERVER] Worker " + socket.id + " failed block (" + workers[socket.id].errors + "). Block re-sent.");
+			var claimed = workers[socket.id].blockclaimed;
+
+			if (typeof(workers[socket.id].timer[claimed]) !== typeof(undefined))
+				clearTimeout(workers[socket.id].timer[claimed]);
+
+			sendBlock(socket, hash, claimed, blocksize);
+		}
+		else
+		{
+			console.log(dateFormat() + "[SERVER] Worker " + socket.id + " failed block (" + workers[socket.id].errors + "). Kicked.");
+			
+			socket.emit('message-received', "[SERVER] You failed after 5 retries. Kicked.");
+			socket.disconnect();
+		}
+	});
+
+	socket.on('hash-found', function(decrypted) {
+		if (verifyHash(decrypted))
+		{
+			console.log(dateFormat() + "[CLIENT] Client id " + socket.id + " decrypted the hash!");
+
+			console.log(dateFormat() + "[SERVER] Saving decrypted hash.");
+			saveHash(decrypted);
+			console.log(dateFormat() + "[SERVER] Loading next hash.");
+			hash = nextHash();
+			console.log(dateFormat() + "[HASH] " + hash);
+
+			if (hash != "")
+			{
+				console.log(dateFormat() + "[SERVER] Canceling the workers.");
+
+				for (var k in workers)
+				{
+					workers[k].ready = true;
+
+					if (typeof(workers[k].blockclaimed) !== typeof(undefined))
+					{
+						var claimed = workers[k].blockclaimed;
+
+						if (typeof(workers[k].timer[claimed]) !== typeof(undefined))
+							clearTimeout(workers[k].timer[claimed]);
+					}
+				}
+
+				io.sockets.emit('block-aborted');
+			}
+			else
+			{
+				console.log(dateFormat() + "[SERVER] No more hash. Stopping the workers.");
+				io.sockets.emit('bruteforce-completed');
+			}
+		}
+		else
+		{
+			socket.emit("[SERVER] Your result is wrong, please contact admin with a paste of hashcat.pot.");
+			socket.disconnect();
+		}
+	});
+
+	socket.on('disconnect', function() {
+
+		if (typeof(workers[socket.id]) != typeof(undefined)) // Check if it's a worker
+		{
+			if (typeof(workers[socket.id].blockclaimed) !== typeof(undefined))
+			{
+				if (workers[socket.id].blockclaimed != null)
+				{
+					var claimed = workers[socket.id].blockclaimed;
+					unresolvedblocks[Object.keys(unresolvedblocks).length] = claimed;
+
+					for (var k in inprogressblocks)
+					{
+						if (inprogressblocks[k] == claimed)
+							inprogressblocks.splice(k, 1);
+					}
+
+					if (typeof(workers[socket.id].timer[claimed]) !== typeof(undefined))
+						clearTimeout(workers[socket.id].timer[claimed]);
+
+					// Fix pool freeze when pool is waiting for the last client and this one left.
+					/*if (blockstart > autoskip && Object.keys(unresolvedblocks).length > 0 && Object.keys(inprogressblocks).length == 0)
+					{
+						for (var k in workers)
+						{
+							if (workers[k].ready && Object.keys(unresolvedblocks).length > 0)
+							{
+								var claimed = unresolvedblocks.shift();
+
+								workers[k].socket.emit('message-received', "[SERVER] Server requested you to compute block until hash skip.");
+								sendBlock(workers[k].socket, hash, claimed, blocksize);
+							}
+						}
+					}*/
+
+					delete workers[socket.id];
+
+					console.log(dateFormat() + "[SERVER] Client " + socket.id + " lost connection. Block " + claimed + " added to queue.");
+				}
+			}
+
+			updateStats(io);
+		}
+	});
+});
+
+var worker = function(socket) {
+	this.socket = socket;
+	this.speed = 0;
+	this.ready = true;
+	this.errors = 0;
+	this.timer = { };
+
+	this.stop = function() {
+
+		console.log(dateFormat() + "[SERVER] Worker " + socket.id + " failed block (Timeout). Kicked.");
+		socket.emit('message-received', "[SERVER] You failed after 30 minutes. Kicked.");
+		socket.disconnect();
+	}
+
+	return this;
+}
+
+function sendBlock(socket, hash, blockstart, blocksize) {
+
+	var blockdata = {
+		hashtodecrypt: hash.toString().trim(),
+		hashtype: '11',
+		attackmode: '3',
+		blockstart: blockstart,
+		blocksize: blocksize - 1
+	};
+
+	workers[socket.id].timer[blockstart] = setTimeout(workers[socket.id].stop, maxresolvetime * 1000);
+	workers[socket.id].blockclaimed = blockstart;
+	workers[socket.id].ready = false;
+
+	socket.emit('block-received', blockdata);
+}
+
+function verifyOs(system) {
+	if (!fs.existsSync("server/os.txt"))
+		fs.writeFileSync("server/os.txt", "");
+
+	var file = fs.readFileSync("server/os.txt").toString().toUpperCase();
+	var os = file.split(';');
+
+	var allowed = false;
+	var system = system.toUpperCase();
+
+	for (var k in os)
+	{
+		if (os[k].toString().trim().indexOf(system) > -1)
+			allowed = true;
+	}
+
+	return allowed;
+}
+
+function verifyHash(decrypted) {
+
+	var md5 = hash.split(':')[0].trim();
+	var salt = hash.split(':')[1].trim();
+	var decrypted = decrypted.trim();
+
+	md5hasher = crypto.createHash('md5');
+	md5hasher.update(decrypted + salt);
+
+	var result = md5hasher.digest('hex');
+
+	if (result == md5)
+		return true;
+	else
+		return false;
+}
+
+function nextHash() {
+	var file = fs.readFileSync("server/hashes.txt");
+	var hashes = file.toString().split('\n');
+
+	blockstart = 0;
+	unresolvedblocks = [ ];
+	inprogressblocks = [ ];
+
+	hashindex++;
+
+	if (typeof(hashes[hashindex - 1]) !== typeof(undefined))
+		return hashes[hashindex - 1].toString().trim();
+	else
+		return "";
+}
+
+function saveAbortedHash(hash) {
+	if (!fs.existsSync("server/aborted.txt"))
+		fs.writeFileSync("server/aborted.txt", "");
+
+	var string = hash + '\n\r';
+
+	fs.appendFileSync("server/aborted.txt", string.toString());
+}
+
+function saveHash(decrypted) {
+	if (!fs.existsSync("server/decrypted.txt"))
+		fs.writeFileSync("server/decrypted.txt", "");
+
+	var string = hash + ":" + decrypted + '\n\r';
+
+	fs.appendFileSync("server/decrypted.txt", string.toString());
+}
+
+function updateStats(io) {
+	poolspeed = 0;
+	onlines = 0;
+
+	for (var k in workers)
+	{
+		if (typeof(workers[k].speed !== typeof(undefined)))
+			if (!isNaN(workers[k].speed))
+				poolspeed += workers[k].speed;
+
+		onlines++;
+	}
+
+	io.sockets.emit('update', { 
+		speed: poolspeed, 
+		onlines: onlines, 
+		hashtodecrypt: hash,
+		blockstart: blockstart,
+		blocksize: blocksize,
+		blockinprogress: Object.keys(inprogressblocks).length,
+		autoskip: autoskip,
+		hashindex: hashindex
+	});
+}
+
+function dateFormat() {
+	var date = new Date();
+
+	return "[" + date.getMonth() + "-" + date.getDate() + "-" + date.getFullYear() + " " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + "]";
+}
