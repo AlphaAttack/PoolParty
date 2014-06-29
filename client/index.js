ea001503e70f40762498ce0a3afb67e5c2f3a382
Stead68 <j0rd4n68@gmail.com>
2014-06-30 00:27:25 +0200

    [v0.11] Beautify the format of date.

--- client/index.js
+++ client/index.js
@@ -195,5 +195,12 @@ function parseSpeed(string) {
 function dateFormat() {
 	var date = new Date();
 
-	return "[" + date.getMonth() + "-" + date.getDate() + "-" + date.getFullYear() + " " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + "]";
+	var year = date.getFullYear();
+	var month = (date.getMonth() < 10) ? "0" + date.getMonth() : date.getMonth();
+	var day = (date.getDate() < 10) ? "0" + data.getDate() : date.getDate();
+	var hours = (date.getHours() < 10) ? "0" + date.getHours() : date.getHours();
+	var minutes = (date.getMinutes() < 10) ? "0" + date.getMinutes() : date.getMinutes();
+	var seconds = (date.getSeconds() < 10) ? "0" + date.getSeconds() : date.getSeconds();
+
+	return "[" + month + "-" + day + "-" + year + " " + hours + ":" + minutes + ":" + seconds + "]";
 }
