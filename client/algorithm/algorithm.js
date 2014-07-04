9b324c2382ab9727ea9897b2c288c8565a856120
Stead68 <j0rd4n68@gmail.com>
2014-07-04 23:01:55 +0200

    Revert "[v0.12] Client: Add md5 & sha1 module."
    This reverts commit 440a08ef1928107d50bf78b0bca3fdc31b983426.

--- client/algorithm/algorithm.js
+++ client/algorithm/algorithm.js
@@ -3,8 +3,6 @@ var fs = require('fs');
 var hashtype;
 
 var joomla = require('./joomla.js');
-var md5 = require('./md5.js');
-var sha1 = require('./sha1.js');
 
 exports.SetHashType = function(hashtype) {
 	this.hashtype = hashtype;
@@ -15,14 +13,8 @@ exports.BruteforceSucceeded = function() {
 	{
 		switch (hashtype)
 		{
-			case 0:
-				return md5.BruteforceSucceeded();
-				break;
 			case 11:
 				return joomla.BruteforceSucceeded();
-				break;
-			case 100:
-				return sha1.BruteforceSucceeded();
 				break;
 		}
 	}
@@ -37,14 +29,8 @@ exports.BruteforceResult = function() {
 	{
 		switch (hashtype)
 		{
-			case 0:
-				return md5.BruteforceResult();
-				break;
 			case 11:
 				return joomla.BruteforceResult();
-				break;
-			case 100:
-				return sha1.BruteforceSucceeded();
 				break;
 		}
 	}
